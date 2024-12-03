b9903fa37d4d6cd9da2e5332d275d051ecf2e23c
SisirSuberi <sisir.suberi@ucalgary.ca>
2024-12-03 13:28:23 -0700

    defined split_dataset function and validate_image function

--- GarbageUtils.py
+++ GarbageUtils.py
@@ -24,3 +24,28 @@ def append_value(dict_obj, key, value):
         dict_obj[key].append(value)
     else:
         dict_obj[key] = [value]
+
+# use random_split to split data into train, val, and test sets
+def split_dataset(dataset, test_size = 0.2, val_size = None):
+    random.shuffle(dataset)
+    
+    data_size = len(dataset)
+    val_size = test_size if val_size is None else val_size
+    train_size = 1 - (test_size + val_size)
+
+    # 0-------a---b----data_size
+    a = int(data_size*train_size)
+    b = int(data_size*(train_size + val_size))
+    train_split = dataset[slice(0, a)]
+    val_split = dataset[slice(a, b)]
+    test_split = dataset[slice(b, -1)]
+
+    return train_split, val_split, test_split
+
+
+def validate_image(path):
+    try:
+        im = Image.open(path)
+        return True
+    except:
+        return False
